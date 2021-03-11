94c776591e43d5d02d2bde9f5f21e1de1402926b
Mark Raudenbush <markraud@hotmail.com>
2021-03-10 23:34:40 -0700

    working on building highScore function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,4 @@
 /*
-Array of questions with 
-Questions 
-Multiple choice answer buttions that highlight when hover
 A final page showing score and asking for your initials to be entered. 
 list of high scores 
 */
@@ -16,62 +13,83 @@ var answerBtns = document.getElementById('answer-btns');
 var timeLeft = 90;
 var score = 0;
 
+
 var myQuestions = [
     {
-        question: 'Commonly used data types DO NOT include: ',
+        question: 'Commonly used data types DO NOT include ________.',
         answers: ['strings','booleans','alerts','numbers'],
         correctAnswer: 'alerts'
     },
     {
-        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is ________.',
         answers: ['JavaScript', 'terminal/bash','for loops','console.log'],
         correctAnswer: 'console.log'
+    },
+    {
+        question: 'The condition in an if / else statement is enclosed within ______.',
+        answers: ['quotes', 'curly brackets','parenthesis','square brackets'],
+        correctAnswer: 'parenthesis'
+    },
+    {
+        question: 'Arrays in JavaScript can be used to store _______.',
+        answers: ['numbers and strings', 'other arrays','booleans','all of the above'],
+        correctAnswer: 'all of the above'
     }
 ]
-
-// console.log(myQuestions[0].question);
-// console.log(myQuestions[0].answers[0].text);
-// console.log(myQuestions[0].answers[0].correct);
-
-
+var quizEnder = myQuestions.length - 1;
 
 function startQuiz() {
     countdown();
     questionEl.classList.remove('hide');
     startInfo.classList.add('hide');
     nextQuestion();
-    return;
+    
 }
 
-function nextQuestion(){   //I'd need to build a loop in here or something
-    document.getElementById('question-text').innerHTML = myQuestions[currentQuestion].question; 
+function nextQuestion(){  
     answerBtns.innerHTML = '';
+    document.getElementById('question-text').innerHTML = myQuestions[currentQuestion].question;
     //loop over all possible choices
     myQuestions[currentQuestion].answers.forEach(function(answer){
-        console.log(answer);
+        //console.log(answer);
         // create a button to put the answers in
         var button = document.createElement('button');
+        button.classList.add('btn');
         button.innerText = answer;
         button.addEventListener("click", checkAnswer);
         answerBtns.appendChild(button);
         
     })
-    
-    return;
+    //console.log('this is users ' + score);
 }
 
 function checkAnswer(){
     if (event.target.innerText === myQuestions[currentQuestion].correctAnswer) {
         console.log('this is correct');
         score++;
+        if (currentQuestion === (myQuestions.length - 1)) {
+            highScore();
+        }  
         currentQuestion++;
         nextQuestion();
     } else {
         console.log('this is wrong');
         timeLeft-10;
+        if (currentQuestion === (myQuestions.length - 1)) {
+            highScore();
+        }
         currentQuestion++;
         nextQuestion();
     }
+return;    
+}
+
+//write function to create another function to  calculate totals and enter initials 
+function highScore() {
+    console.log('build the high score');
+    // remove the question and add High Score
+    //create box to put initials in and log
+    //display initials and score
 }
 
 // Timer that counts down from 90
